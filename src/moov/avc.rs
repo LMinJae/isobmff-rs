9330d15d45cc0841a495a1768458446bac8cbf42
Min-Jae, Lee <lee.min.jae.96@gmail.com>
2022-03-10 19:42:09 +0900

    Remove high profile, due to don't know condition

--- src/moov/avc.rs
+++ src/moov/avc.rs
@@ -14,8 +14,6 @@ pub struct avcC {
     pub(crate) length_size_minus_one: u8,
     pub(crate) sps: Vec<BytesMut>,
     pub(crate) pps: Vec<BytesMut>,
-
-    pub(crate) ext_high_profile: Option<(u8, u8, u8, Vec<BytesMut>)>,
 }
 
 impl Debug for avcC {
@@ -32,16 +30,6 @@ impl Debug for avcC {
         f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\tnb_pps: {:?}", self.pps.len()))?;
         for it in &self.pps {
             f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\t\t{:x?}", it))?;
-        }
-
-        if let Some((chroma_format, bit_depth_luma_minus8, bit_depth_chroma_minus8, sps_ext)) = &self.ext_high_profile {
-            f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\tchroma_format: {:?}", chroma_format))?;
-            f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\tbit_depth_luma_minus8: {:?}", bit_depth_luma_minus8))?;
-            f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\tbit_depth_chroma_minus8: {:?}", bit_depth_chroma_minus8))?;
-            f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\tnb_spse: {:?}", sps_ext.len()))?;
-            for it in sps_ext {
-                f.write_fmt(format_args!("\n\t\t\t\t\t\t\t\t\t\t\t{:x?}", it))?;
-            }
         }
 
         Ok(())
@@ -70,23 +58,6 @@ impl IO for avcC {
             pps.push(r.split_to(len as usize));
         }
 
-        let ext_high_profile = if 100 == profile_indication || 110 == profile_indication ||
-            122 == profile_indication || 144 == profile_indication {
-            let chroma_format = r.get_u8() & 0b11;
-            let bit_depth_luma_minus8 = r.get_u8() & 0b111;
-            let bit_depth_chroma_minus8 = r.get_u8() & 0b111;
-            let nb_sps_ext = r.get_u8() & 0b11111;
-            let mut sps_ext = Vec::with_capacity(nb_sps_ext as usize);
-            for _ in 0..nb_sps_ext {
-                let len = r.get_u16();
-                sps_ext.push(r.split_to(len as usize));
-            }
-
-            Some((chroma_format, bit_depth_luma_minus8, bit_depth_chroma_minus8, sps_ext))
-        } else {
-            None
-        };
-
         Self {
             configuration_version,
             profile_indication,
@@ -95,7 +66,6 @@ impl IO for avcC {
             length_size_minus_one,
             sps,
             pps,
-            ext_high_profile,
         }
     }
 
@@ -118,20 +88,6 @@ impl IO for avcC {
         for it in &self.pps {
             w.put_u16(it.len() as u16);
             w.put(it.chunk());
-        }
-
-        if 100 == self.profile_indication || 110 == self.profile_indication ||
-            122 == self.profile_indication || 144 == self.profile_indication {
-            if let Some((chroma_format, bit_depth_luma_minus8, bit_depth_chroma_minus8, sps_ext)) = &self.ext_high_profile {
-                w.put_u8((chroma_format & 0b11) | 0b11111100);
-                w.put_u8((bit_depth_luma_minus8 & 0b111) | 0b11111000);
-                w.put_u8((bit_depth_chroma_minus8 & 0b111) | 0b11111000);
-                w.put_u8((sps_ext.len() & 0b11111) as u8 | 0b11100000);
-                for it in sps_ext {
-                    w.put_u16(it.len() as u16);
-                    w.put(it.chunk());
-                }
-            }
         }
 
         w
