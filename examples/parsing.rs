1264231bdeb52f1ee3e3db38c211809d23f20fa7
Min-Jae, Lee <lee.min.jae.96@gmail.com>
2022-03-11 16:54:46 +0900

    print mdat relative offset for debug

--- examples/parsing.rs
+++ examples/parsing.rs
@@ -24,18 +24,20 @@ fn parse(mut buf: BytesMut) {
     let mut moof = isobmff::moof::moof::default();
 
     let mut offset = 0;
+    let mut moof_base_offset = 0;
 
     while 0 < buf.len() {
-        let cur_offset = offset;
         let tmp = buf[0];
 
         let mut b = isobmff::Object::parse(&mut buf);
 
-        eprintln!("[0x{:08x?}] 0x{:08x?}: {:?}", cur_offset, b.box_type, std::str::from_utf8(&b.box_type.to_be_bytes()).unwrap_or(""));
-        offset += 8 + b.payload.len();
+        eprintln!("[0x{:08x?}] 0x{:08x?}: {:?}", offset, b.box_type, std::str::from_utf8(&b.box_type.to_be_bytes()).unwrap_or(""));
+        offset += 8;
         if 0 == tmp {
             offset += 8;
         }
+        let cur_offset = offset;
+        offset += b.payload.len();
         match b.box_type {
             // ftyp: File Type
             isobmff::ftyp::ftyp::BOX_TYPE => {
@@ -65,6 +67,18 @@ fn parse(mut buf: BytesMut) {
             }
             // mdat: Media Data
             0x6d646174 => {
+                let diff = (cur_offset - moof_base_offset);
+
+                for traf in moof.trafs.iter_mut() {
+                    for trun in traf.truns.iter_mut() {
+                        let mut data_offset = (traf.tfhd.base_data_offset.unwrap_or(0) as i64 + trun.data_offset.unwrap_or(0) as i64 - diff as i64) as u64;
+                        for (duration, size, flags, composition_time_offset) in trun.samples.iter_mut() {
+                            eprintln!("0x{:08x?}: {:?} {:?} {:08x?} {:?}", data_offset, duration, size, flags, composition_time_offset);
+                            data_offset += size.unwrap_or(traf.tfhd.default_sample_size.unwrap()) as u64;
+                        }
+                    }
+                }
+
                 let mut w = BytesMut::with_capacity(16 + moof.len() + b.payload.len());
 
                 w.put(Object {
